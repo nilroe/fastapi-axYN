60813b1ee7e031531f665e63627280f05bb16e56
xavi <xalvarezs@romeu.com>
2023-03-18 13:05:51 +0100

    python-jose and push to DETA

--- config/handler.py
+++ config/handler.py
@@ -1,6 +1,5 @@
 import time
 from typing import Dict
-
 from jose import jwt
 
 JWT_SECRET="GsTbHGLBC1ZVp1wNiJhNy6Cw?Qznu!cMcOMz/fAj2XR8MPDUvANxFBC7O-AhLKCX1o9V0Q5==QI!xJPUCHBvb/qhu/zaG1I4lFNSo1LecVaLfe9rxU5ImukKuU0qTBBdE0PjMUyHp7S0VB0/wXuk-9Jnm/Cfy5yYW?z-TLfr!PJvKXvI2UFifbRE5CIOMSu!01oloV6/P2gxBYVkBJJEuAB4L-egB0Ta09lG79!Zd7gRm6uzL01X/t3/RBehj3Gi"

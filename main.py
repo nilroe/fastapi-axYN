2457c4ac4ce7974645259e87e02a3125f4c26132
xavi <xalvarezs@romeu.com>
2023-03-19 07:47:06 +0100

    CORS

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from schema import crud
+from starlette.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()           
 
 app.include_router(crud.crud)
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
